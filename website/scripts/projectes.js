924b1cf1513d6d3e3c5432e8ae51fcf15712f374
unknown <albert@albert-PC.(none)>
2012-08-09 23:47:16 +0200

    all website

---
+++ website/scripts/projectes.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+    var imatgePetitaAnterior = $('#imatgesProjectes .contenidorImatges img').attr('src');
+    $('#imatgesProjectes .contenidorImatges li a').mouseover(function () {
+        $('#imatgesProjectes .imatgeGran img').attr('src', $(this).find('img').attr('src'));
+    });
+    $('#imatgesProjectes a').mouseout(function () {
+        $('#imatgesProjectes .imatgeGran img').attr('src', imatgePetitaAnterior);
+    });
+
+    //$('#menuProjectes').tinyscrollbar({ axis: 'x' });
+
+    var imatgePreviaAnterior = $('#imatgesProjectes .contenidorImatges img').attr('src');
+    var $imatgePrevia = $('#menuProjectes .petites');
+    $imatgePrevia.find("a").mouseover(function () {
+        var nodeId = $(this).attr('data-id');
+        //alert(nodeId);
+        $('.descripcioProjecte').addClass("hide");
+        $('.contenidorImatges').addClass("hide");
+        $('#imatgesProjectes .imatgeGran img').attr('src', $("#menuProjectes .petites a[data-id =" + nodeId + "]").find("img").attr('src'));
+    });
+    $('#menuProjectes .petites li').mouseleave(function () {
+        $('.descripcioProjecte').removeClass("hide");
+        $('.contenidorImatges').removeClass("hide");
+        $('#imatgesProjectes .imatgeGran img').attr('src', imatgePreviaAnterior);
+    });
+
+});
+
---
+++ website/scripts/publicacions.js
@@ -0,0 +1,11 @@
+﻿$(window).load(function () {
+    $('#publicacions .projecte').isotope({ filter: ".publicacionsPremis" });
+    $('#publicacions .projecte div').each(function () {
+        $(this).removeClass("hide");
+    });
+    $('#filters a').click(function () {
+        var selector = $(this).attr('data-filter');
+        $('#publicacions .projecte').isotope({ filter: selector });
+        return false;
+    });
+});
